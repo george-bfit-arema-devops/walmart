52db67540422a53b57d383d2f465181c565f17a8
georgebfitarena <105264659+georgebfitarena@users.noreply.github.com>
2022-06-13 21:50:24 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,2 +1,4 @@
 This is a python file
 it is created in master branch
+this file is updated in stage branch on 13 jun 22
+this file is updated to check pull request from one branch to other branch remotely
